d4f9bed1a34c7758e7dca48af532f407566fa540
Sebahattin KALACH <sepco.kilic@gmail.com>
2021-02-21 13:27:17 +0000

    SK - Webpackconfig

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 // CSS 
 import logo from '../../../logo.svg';
-import './App.css'
+import classes from './App.css'
 
 export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={logo} className={classes.AppLogo } alt="logo" />
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
