b6f3030f8a630356ae6463b717d29b8a8341d379
Sebahattin KALACH <sepco.kilic@gmail.com>
2021-05-04 16:46:53 +0100

    Spell Check

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
         return (
             <nav className="navbar navbar-light bg-light left">
                 <div className="container-fluid">
-                    <p>Developt by Sebahattin KALACH</p>
+                    <p>Developed by Sebahattin KALACH</p>
                 </div>
             </nav>
         )
